bbdb659f1887b90c06a5dee7f2dd1eb408225b3a
Jared Kozar <jared.rkozar@gmail.com>
2023-07-13 19:18:47 -0400

    disables save tag button if tag name is empty

--- SimpleNotes/Views/NewTagView.swift
+++ SimpleNotes/Views/NewTagView.swift
@@ -43,6 +43,7 @@ struct NewTagView: View {
                     }) {
                        Text("Save")
                     }
+                    .disabled(tagProperties.tagName.isEmpty)
                 }
             }
             .accentColor(tagProperties.tagColor)
